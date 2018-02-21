6dd4ed6352811a2d63b06399184ada0f1b55ddb8
AlexDid <did-aleksej@yandex.ru>
2018-02-21 23:10:23 +0200

    update app module and tsconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import {Module} from '@nestjs/common';
 import {AuthenticationModule} from "./modules/authentication/authentication.module";
 import {UsersModule} from './modules/users/users.module';
 import {DebtsModule} from './modules/debts/debts.module';
+import {OperationsModule} from './modules/operations/operations.module';
 
 @Module({
     modules: [
         AuthenticationModule,
         UsersModule,
-        DebtsModule
+        DebtsModule,
+        OperationsModule
     ]
 })
 export class ApplicationModule {}
