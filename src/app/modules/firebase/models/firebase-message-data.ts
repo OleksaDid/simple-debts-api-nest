8fc91fdf1a48ecb790476f87624c3fb3a8f39ee2
AlexDid <did-aleksej@yandex.ru>
2020-06-08 16:53:43 +0545

    add click_action to push notification payload

--- src/app/modules/firebase/models/firebase-message-data.ts
+++ src/app/modules/firebase/models/firebase-message-data.ts
@@ -1,6 +1,6 @@
 export class FirebaseMessageData {
   debtsId: string;
-  click_action = 'FLUTTER_NOTIFICATION_CLICK';
+  readonly click_action = 'FLUTTER_NOTIFICATION_CLICK';
 
   constructor(debtsId: string) {
     this.debtsId = debtsId;
